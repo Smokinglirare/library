306b9f72a2ef381773049bee88323255f8a67b57
Sebastian <sebastian.thorneus@gmail.com>
2022-06-13 16:26:56 +0100

    allt tycks fungera bra, final commit

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,122 +1,109 @@
-
-
 const model = require("../models/books.models");
 
+async function getBooks(req, res) {
+  const result = await model.findAll();
 
+  if (result.length === 0) {
+    return res.status(404).json({ error: "Cannot find any books" });
+  }
 
-
-
- async function getBooks(req, res) {
-    const result = await model.findAll();
-
-   if (result.length === 0) {
-		return res.status(404).json( {error: "Cannot find any books"} );
-	}
-
-    res.json({Succes: result});
+  res.json({ Success: result });
 }
 
-
 async function getBook(req, res) {
-    const result = await model.findOne(req.params.id);
+  const result = await model.findOne(req.params.id);
 
-    if (!result) {
-        return res.status(404).json({error: "Book doesn't exist"});
-      }
-    
-    res.json({Success: result});
-    
+  if (!result) {
+    return res.status(404).json({ error: "Book doesn't exist" });
+  }
+
+  res.json({ Success: result });
 }
 
 async function deleteBook(req, res) {
-    const result = await model.findOne(req.params.id);
+  const result = await model.findOne(req.params.id);
 
-    if (!result) {
-        return res.status(404).json({error: "Book doesn't exist"});
-      }
+  if (!result) {
+    return res.status(404).json({ error: "Book doesn't exist" });
+  }
 
-    await model.deleteOne(req.params.id);
+  await model.deleteOne(req.params.id);
 
-    res.json({Success: "Deleted book"});
-} 
+  res.json({ Success: "Deleted book" });
+}
 
 async function addBook(req, res) {
-    const {name, author} =  req.body;
+  const { name, author } = req.body;
 
-    const newBook = {
-        name,
-        author
-    }
+  const newBook = {
+    name,
+    author,
+  };
 
-    if (!name || !author ) {
-		 return res.status(400).json({error: "You have to insert both name and author"});
-		
-	}
+  if (!name || !author) {
+    return res
+      .status(400)
+      .json({ error: "You have to insert both name and author" });
+  }
 
   await model.addOne(newBook);
-    res.json({DudeBigSuccess: "Added book"});
+  res.json({ DudeBigSuccess: "Added book" });
 }
 
 async function updateBook(req, res) {
-    
-    const id = req.params.id;
-    const result = await model.findOne(req.params.id);
+  const id = req.params.id;
+  const result = await model.findOne(req.params.id);
 
+  if (!result) {
+    return res.status(404).json({ error: "Book doesn't exist" });
+  }
 
-    if (!result) {
-        return res.status(404).json({error: "Book doesn't exist"});
-	}
+  if (!req.body.name || !req.body.author) {
+    return res
+      .status(400)
+      .json({ error: "You need to insert both name and author of book" });
+  }
 
-    if (!req.body.name || !req.body.author){
-        return res.status(400).json({error: "You need to insert both name and author of book"});
-    }
-
-    await model.updateOne(id ,req.body);
-    res.json({ Success: "Changed book"});
+  await model.updateOne(id, req.body);
+  res.json({ Success: "Changed book" });
 }
 
 async function updateDetailOfBook(req, res) {
-    const id = req.params.id;
-    const existingBook = await model.findOne(id);
+  const id = req.params.id;
+  const existingBook = await model.findOne(id);
 
-    const newBook = {
-        ...existingBook,
-    }
+  const newBook = {
+    ...existingBook,
+  };
 
-    if (req.body.name) {
-        newBook.name = req.body.name;
-    }
+  if (req.body.name) {
+    newBook.name = req.body.name;
+  }
 
-    if (req.body.author) {
-        newBook.author = req.body.author;
-    }
-    
-    if (!existingBook) {
-        return res.status(404).json({error: "Book doesn't exist"});
-      }
+  if (req.body.author) {
+    newBook.author = req.body.author;
+  }
 
-    if (!req.body.name && !req.body.author) {
-        return res.status(400).json({error: "You have to change either name or author."});
-      }
+  if (!existingBook) {
+    return res.status(404).json({ error: "Book doesn't exist" });
+  }
 
-   
-     await model.updateOne(id, newBook);
+  if (!req.body.name && !req.body.author) {
+    return res
+      .status(400)
+      .json({ error: "You have to change either name or author." });
+  }
 
+  await model.updateOne(id, newBook);
 
-    res.json({Succes:"Changed book"});
-
+  res.json({ Succes: "Changed book" });
 }
-
-
- 
 
 module.exports = {
-    getBooks,
-    getBook ,
-    deleteBook,
-    addBook,
-    updateBook,
-    updateDetailOfBook
-  
-    
-}
+  getBooks,
+  getBook,
+  deleteBook,
+  addBook,
+  updateBook,
+  updateDetailOfBook,
+};
